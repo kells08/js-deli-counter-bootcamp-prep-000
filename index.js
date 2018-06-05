f42c9d8f86f156312f323292346a1fd403dbd8a0
Kelly Lwakatare <devkells00@gmail.com>
2018-06-05 20:00:08 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
+  
+  return `Welcome, ${name}. You are ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  return 
+}
